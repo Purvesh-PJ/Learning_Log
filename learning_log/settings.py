097f090b301b8b40e5af80ce8fb14c0629f475f6
Purvesh_PJ <Purvesh9997@gmail.com>
2022-04-20 15:04:49 +0530

    Ready

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -1,4 +1,3 @@
-import django_heroku
 """
 Django settings for learning_log project.
 
@@ -137,7 +136,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'users:login'
 
 # Heroku settings
-
+import django_heroku
 django_heroku.settings(locals())
 
 
